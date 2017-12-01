77368c3a68175dea03dee4dbd44c901bbb3b39b5
timo <tmhermans@gmail.com>
2017-12-01 21:23:56 +0100

    add character-sheet component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterSheetComponent
   ],
   imports: [
     BrowserModule

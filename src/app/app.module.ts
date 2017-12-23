76a869654fcf8f64312165a277a2b0559cceab3e
Timo Hermans <tmhermans@gmail.com>
2017-12-23 09:54:31 +0100

    fix the error on app initialization (loggedInUser)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,8 +42,6 @@ export function initFirebase() {
               email: user.email,
               token: accessToken
             };
-
-            this.userLoggedIn = userLoggedIn;
           });
         }
         resolve();
